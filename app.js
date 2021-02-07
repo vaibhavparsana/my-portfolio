57287c1fa492e1b124883ee2b5cf1b07eef290d3
vaibhavparsana <parsana7v@gmail.com>
2021-02-07 18:53:36 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const path = require('path');
+
+
+const app = express();
+
+
+
+var os = require('os');
+
+
+
+var networkInterfaces = os.networkInterfaces();
+
+
+
+//hello world
+
+// Serve the static files from the React app
+
+app.use(express.static(path.join(__dirname, 'react-app/build')));
+
+
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port);
+
+console.log('App is listening on port ' + port);
